8dd5d160f8d07e41da7b50ae329b1aca0766a057
suraj-maniyar <spmaniyar7@gmail.com>
2018-06-29 20:52:53 -0400

    Restructured code

--- install.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install python-numpy
-sudo -H pip install matplotlib
-sudo -H pip install sklearn
-sudo apt-get install libopencv-dev python-opencv
-
--- main.py
+++ main.py
@@ -1,90 +1,114 @@
-
-from include.DataHandling import get_image_train2, get_image_CV2 
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
-import cPickle as pickle
+from PIL import Image
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+import pickle
 
-tf.logging.set_verbosity(tf.logging.INFO)
+# Change this path to your FDDB Dataset path
+FDDB_path = '/home/suraj/Dataset/FDDB'
 
-len_train = 6000  
-len_CV = 600                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
-gray = 0
-img_size = 60
-n_channels = 3
-if gray:
-    n_channels = 1
+annotation_folder = FDDB_path+'/FDDB-folds/'
+data_folder = FDDB_path+'/originalPics/'
 
-lr = 0.0007983
+X_train = []
+Y_train = []
+img_size = 200
+
+
+def get_coords(line):
+    line = line.replace('\n','')   
+    line = line.split(' ')
+    line.remove('')
+    line = [float(i) for i in line]    
+    [major_axis_radius, minor_axis_radius, angle, center_x, center_y, _] = line
+
+    # The FDDB dataset gives the face annotation in the form of an ellipse.
+    # We take it's end points and consider it as a rectangle 
+    x = int(center_x - minor_axis_radius)
+    y = int(center_y - major_axis_radius)
+    w = int(2*minor_axis_radius)
+    h = int(2*major_axis_radius)
+    
+    return [x, y, w, h]
+
+
+for index in range(1, 11):
+    print("index = ", index)
+    file_name = 'FDDB-fold-'+str(index).zfill(2)+'-ellipseList.txt'
+    file_path = FDDB_path+'/FDDB-folds/'+file_name
+    fp = open(file_path, 'r')
+    lines = fp.readlines()
+
+    for i in range (len(lines)):
+        line = lines[i].replace('\n','')
+        
+        # Images which have only one face
+        if(line == '1'):
+            image_path = lines[i-1].replace('\n','')+'.jpg'
+            image_path = data_folder+image_path
+            line_coords = lines[i+1]
+            [x,y,w,h] = get_coords(line_coords)
+            img = Image.open(image_path)
+            [W, H] = img.size
+            
+            img_face = img.crop((x,y,x+w,y+h))
+
+            # crop face image
+            # crop random area from original image not contatining any face              
+            img_nonface = img.crop((x+w, y+h, W, H))
+            
+            img_face = img_face.resize((img_size,img_size))
+            img_nonface = img_nonface.resize((img_size,img_size))
+            img_face = np.array(img_face)
+            img_nonface = np.array(img_nonface)
+            
+            # Check is the image is RGB
+            if(img_face.shape == (img_size,img_size,3)):
+               X_train.append(img_face)
+               Y_train.append([1,0])
+               X_train.append(img_nonface) 
+               Y_train.append([0,1])
+               
+               
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
+
+X_train, X_CV, Y_train, Y_CV = train_test_split(X_train, Y_train, test_size=0.20, random_state=42)  
+
+print(X_train.shape, Y_train.shape)
+print(X_CV.shape, Y_CV.shape)
+
+
+
+
+learning_rate = 0.0007983
 epochs = 10
 batch_size = 64
-n_classes = 2
-drop_out_keep_prob = 0.8
-dp = 1 - drop_out_keep_prob
-reg = 0.0100286
+n_classes = 2              # num of output classes
+drop_out = 0.2             # Drop-Out probability (Hyperparameter)
+reg = 0.0100286            # Regularization constant (Hyperparameter)
+n_channels = 3             # RGB Image
+len_train = X_train.shape[0]
+len_CV = X_CV.shape[0]
+
 
 def print_params():
-    print('lr: ', lr, 'reg', reg)
+    print('Learning Rate: ', learning_rate)
+    print('Regularization:', reg)
+        
 
 def log_parameters():
-    arr = [len_train, len_CV, lr, epochs, batch_size, drop_out_keep_prob, reg]        
+    arr = [len_train, len_CV, learning_rate, epochs, batch_size, 1-drop_out, reg]        
     np.savetxt('Results/params.txt', arr)                                                                                                                                                                                   
-    print "Parameters logged"
+    print("Parameters logged")
     
 
 def save(arr):
-    print "Dumping in a file"
+    print("Dumping to a file")
     with open('data.p', 'wb') as f:
         pickle.dump(arr, f)
-    print "Saved"    
-
-def load_data():
-    print "Loading from pickle file"
-    with open('data.pkl', 'rb') as f:
-        [X_train, Y_train, X_CV, Y_CV] = pickle.load(f)
-    print "Data Loaded"    
-    return [X_train, Y_train, X_CV, Y_CV]    
-
-print "Initializing"
-
-
-def get_data(len_train=len_train, len_CV=len_CV, gray=gray, n_channels=n_channels):
-    X_train = np.zeros((2 * len_train, img_size, img_size, n_channels))
-    X_CV = np.zeros((2 * len_CV, img_size, img_size, n_channels))
-    Y_train = np.zeros((2*len_train, 2))
-    Y_CV = np.zeros((2*len_CV, 2))
-
-    print "Loading Train Data"
-    for i in range(0, len_train):
-        #print i+1 ,"/",len_train
-        X_train[2*i] = get_image_train2(i, 'face', gray=gray)
-        Y_train[2*i] = np.array([1,0])
-        X_train[2*i+1] = get_image_train2(i, 'nonface', gray=gray)
-        Y_train[2*i+1] = np.array([0,1])
-    
-    print "Loading Test Data"
-    for i in range(0, len_CV):
-        #print i+1, "/", len_CV
-        X_CV[i] = get_image_CV2(i, 'face', gray=gray)
-        X_CV[i + len_CV] = get_image_CV2(i, 'nonface', gray=gray)
-        Y_CV[i] = np.array([1, 0])
-        Y_CV[i + len_CV] = np.array([0, 1])
-
-
-    X_train /= 255.0
-    X_CV /= 255.0
-     
-    arr = [X_train, Y_train, X_CV, Y_CV] 
-    return arr
-
-
-[X_train, Y_train, X_CV, Y_CV] = get_data(len_train=len_train, len_CV=len_CV, gray=gray, n_channels=n_channels)
-
-print "shuffling"
-X_train, X_, Y_train, Y_ = train_test_split(X_train, Y_train, test_size=0, random_state=42)
-
+    print("Saved")    
 
 
 
@@ -92,7 +116,8 @@ X_train, X_, Y_train, Y_ = train_test_split(X_train, Y_train, test_size=0, rando
 ##########################################################################################################################
 
 
-print "Build model"
+print("Building model")
+
 
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
@@ -120,9 +145,8 @@ W_conv1 = weight_variable([5, 5, n_channels, 16])
 b_conv1 = bias_variable([16])
 W_conv2 = weight_variable([5, 5, 16, 16])
 b_conv2 = bias_variable([16])
-W_fc1 = weight_variable([15*15*16, 128])
-#W_fc1 = weight_variable([30*30*16, 100])
-b_fc1 = bias_variable([128])
+W_fc1 = weight_variable([50*50*16, 64])
+b_fc1 = bias_variable([64])
 
 
 h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)
@@ -131,18 +155,20 @@ h_pool1 = max_pool_2d(h_conv1)
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2d(h_conv2)
 
-flattened = tf.reshape(h_pool2, [-1, 15*15*16])
+flattened = tf.reshape(h_pool2, [-1, 50*50*16])
 
 h_fc1 = tf.nn.relu(tf.matmul(flattened, W_fc1) + b_fc1)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+h_fc1_drop = tf.nn.dropout(h_fc1, 1-drop_out)
 
-W_fc2 = weight_variable([128, n_classes])
+W_fc2 = weight_variable([64, n_classes])
 b_fc2 = bias_variable([n_classes])
 
 y_ = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 prediction = tf.nn.softmax(y_)
 
-total_batches = 2*len_train/batch_size
+total_batches_train = int(len_train/batch_size)
+total_batches_CV = int(len_CV/batch_size)
+
 
 sess = tf.Session()
 
@@ -151,7 +177,7 @@ cross_entropy = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits_v2(label
                    reg*(tf.nn.l2_loss(W_conv1) + tf.nn.l2_loss(W_conv2) + \
                            	       tf.nn.l2_loss(W_fc1) + tf.nn.l2_loss(W_fc2) )
 
-train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
 
 "Evaluate the model"
 correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
@@ -167,26 +193,46 @@ epoch_arr = []
 print_params()
 
 for epoch in range(epochs):
+    
+    # Average Cost
     avg_cost = 0
-    for i in range(total_batches):
+    for i in range(total_batches_train):
         batch_x = X_train[i*batch_size : (i+1)*batch_size]        
         batch_y = Y_train[i*batch_size : (i+1)*batch_size] 
+        
         _, c = sess.run([train_step, cross_entropy], 
-                        feed_dict={ x: batch_x, y: batch_y, keep_prob: drop_out_keep_prob })
-        avg_cost += c/total_batches    
-    acc_train = sess.run(accuracy, feed_dict={x: X_train[0:1000], y: Y_train[0:1000], keep_prob:1.0})
-    acc_test = sess.run(accuracy, feed_dict={x: X_CV, y: Y_CV, keep_prob:1.0})    
+                        feed_dict={ x: batch_x, y: batch_y, keep_prob: 1-drop_out })
+        avg_cost += c/total_batches_train    
+    
+    # Training Accuracy
+    acc_train = 0
+    for i in range(total_batches_train):
+        batch_x = X_train[i*batch_size : (i+1)*batch_size]        
+        batch_y = Y_train[i*batch_size : (i+1)*batch_size] 
+        acc_train += sess.run(accuracy, feed_dict={x: batch_x, y: batch_y, keep_prob:1.0}) / total_batches_train           
+    
+    # Validation Accuracy 
+    acc_val = 0
+    for i in range(total_batches_CV):
+        batch_x = X_CV[i*batch_size : (i+1)*batch_size]        
+        batch_y = Y_CV[i*batch_size : (i+1)*batch_size] 
+        acc_val += sess.run(accuracy, feed_dict={x: batch_x, y: batch_y, keep_prob:1.0}) / total_batches_CV
+    
+    
     epoch_arr.append(epoch+1)
     cost_arr.append(avg_cost)
     train_acc_arr.append(acc_train)
-    test_acc_arr.append(acc_test)
+    test_acc_arr.append(acc_val)
+
+
     results = [epoch_arr, train_acc_arr, test_acc_arr, cost_arr]
     results = np.array(results)
     with open('Results/results.pkl', 'wb') as f:
         pickle.dump(results, f)
 
-    print("Epoch:", epoch+1, "cost:", avg_cost, "Train accuracy:", acc_train, "Test accuracy:", acc_test)
+    print("Epoch:", epoch+1, "cost:", avg_cost, "Train accuracy:", acc_train, "Val accuracy:", acc_val)
 
+    
 log_parameters()
 
 
@@ -208,6 +254,38 @@ plt.legend(loc=0)
 p2.savefig('Results/Loss.png')
 
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
